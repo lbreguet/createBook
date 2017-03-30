543143c2a18266582211d9e9d942d82324d6f861
lbreguet <louis-w@live.com>
2017-03-30 13:36:58 -0400

    Comments on show post
    -comments take the post id to generate all comments associated with it
    -every new comment we does the show action on the selected post
    -color schemes and border-radius's

--- assets/scripts/comments/events.js
+++ assets/scripts/comments/events.js
@@ -4,6 +4,9 @@ const getFormFields = require(`../../../lib/get-form-fields`)
 
 const api = require('./api')
 const ui = require('./ui')
+const postApi = require('../posts/api')
+const postUi = require('../posts/ui')
+const postEvents = require('../posts/events')
 
 const onCreateComment = (event) => {
   event.preventDefault()
@@ -11,7 +14,11 @@ const onCreateComment = (event) => {
   const id = event.target.dataset.id
   api.create(data, id)
     .then(ui.createSuccess)
-    .catch(ui.failure)
+    .then(() => postApi.show(id)
+      .then(postUi.showSuccess)
+      .then(api.index(id)
+            .then((data) => ui.indexSuccess(data, id))))
+      .catch(ui.failure)
 }
 
 const addHandlers = function () {
--- assets/scripts/comments/ui.js
+++ assets/scripts/comments/ui.js
@@ -3,7 +3,6 @@
 const indexCommentTemplate = require('../templates/comments/index-comment.handlebars')
 
 const indexSuccess = function (data, id) {
-  console.log(data, 'DATA')
   const indexCommentHtml = indexCommentTemplate({ comments: data.comments, post_id: id })
   $('.index-comment').empty().append(indexCommentHtml)
 }
